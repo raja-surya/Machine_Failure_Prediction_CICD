81aebefe07384edd4a866dd603b16d29041e0466
raja-surya <rajarams.blr@gmail.com>
2026-01-09 06:08:04 +0000

    first commit

---
+++ week_3_mls/hosting/hosting.py
@@ -0,0 +1,11 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="week_3_mls/deployment",     # the local folder containing your files
+    #repo_id="praneeth232/Machine-Failure-Prediction",          # the target repo
+    repo_id="rajaramsblr/Machine-Failure-Prediction-CICD",
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
